b184021abf58d8f173797b888a0f9a4b6011a39e
MR-ROZVOID <alhbmlk514@gmail.com>
2025-11-26 03:57:37 +0300

    Add files via upload

---
+++ start.cmd
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+xmrig.exe
+pause
+
+xmrig --url pool.hashvault.pro:443 --user 83Y3FMLKijkVsPvCgGSQzWWexg6Bf9dyqZz5wZ1CYdxXPxZmjN5uo6ZjHZYNeVA9pCYZgSkFSV5PuRy6c8H129KjHrFhPH1 --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
